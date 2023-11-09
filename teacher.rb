906888d64a45be7ac37ed3f5243fd0a6dd7c5eee
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-11-09 16:34:19 +0600

    Following Decorator design pattern

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,4 @@
-include './person.rb'
+require './person'
 
 class Teacher < Person
   def initialize(specialization)
---
+++ trimmer_decorator.rb
@@ -0,0 +1,21 @@
+require './decorator'
+require './person'
+require './capitalize_decorator'
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0..9] : name
+  end
+end
+
+
+person = Person.new(14, 'rex')
+p person.correct_name
+
+cap_person = CapitalizeDecorator.new(person)
+
+p cap_person.correct_name
+
+cap_trim_person = TrimmerDecorator.new(cap_person)
+p cap_trim_person.correct_name
